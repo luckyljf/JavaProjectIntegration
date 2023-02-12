26fab3ffd3ca079066f755265486e175e018c7e5
ljf <luckylijf@gmail.com>
2023-02-12 15:36:05 +0800

    Java_RocketMQ项目初始化

---
+++ Java_RocketMQ/src/main/java/com/ljf/rocketmq/Main.java
@@ -0,0 +1,12 @@
+package com.ljf.rocketmq;
+
+/**
+ * @author ljf
+ * @date ${DATE} ${TIME}
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
